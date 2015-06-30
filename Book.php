78446e604d76bc1aef2a43bb1c489ffb218e1164
przemek-kopaczewski <kopaczewski.przemek@gmail.com>
2015-06-30 10:13:18 +0200

    library

---
+++ Book.php
@@ -0,0 +1,42 @@
+<?php
+
+class Book
+{
+    private $id;
+    private $title;
+    private $author;
+    private $type;
+    
+    public function __construct($id, $title, $author, $type) 
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->author = $author;
+        $this->type = $type;
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+    
+    public function getType()
+    {
+        return $this->type;
+    }
+    
+    public function getFullBook() 
+    {
+        return $this->id . ' ' . $this->title . ' ' . $this->author . ' ' . $this->type;
+    }
+}
